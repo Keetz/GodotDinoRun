f96db2e40e1f330b2e47b75b7a96cdc10cb707c8
Laurent Van Acker <laurent@pozyx.io>
2018-02-06 15:03:03 +0100

    Added basic obstacle

--- GodotDinoRun/src/level.gd
+++ GodotDinoRun/src/level.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 onready var score_text = get_node("Score")
+onready var obstacle_manager = get_node("PlayArea/ObstacleManager")
 
 onready var spawn_timer = Timer.new()
 
@@ -10,12 +11,26 @@ func set_score():
 	score_text.set_score(int(score))
 
 func _ready():
+	add_child(spawn_timer)
 	spawn_timer.connect("timeout", self, "_on_spawn_timer_timeout")
+	# long wait time before first obstacle
+	spawn_timer.set_wait_time(4)
+	spawn_timer.start()
+	
 	score_text.reset_score()
 	set_score()
 	
-	
+func reset_spawn_timer():
+	spawn_timer.set_wait_time(get_random_timeout())
+	spawn_timer.start()
+
+func get_random_timeout():
+	return 1 + 1 * randf()
 	
 func _process(delta):
 	score += delta * 1000
 	set_score()
+	
+func _on_spawn_timer_timeout():
+	obstacle_manager.spawn_obstacle()
+	reset_spawn_timer()
--- GodotDinoRun/src/obstacles/obstacle.gd
+++ GodotDinoRun/src/obstacles/obstacle.gd
@@ -1,4 +1,4 @@
-extends Sprite
+extends Node2D
 
 func _ready():
 	pass
